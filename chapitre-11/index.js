c32644b40562b4e63a6255eb790a682a58714677
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-03-14 09:13:23 +0100

    dom

--- chapitre-11/index.js
+++ chapitre-11/index.js
@@ -1,1 +1,75 @@
-console.log("chapitre 11");
+// anairi@esgi.fr
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement
+// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+
+// Récupérer un élément par son identifiant
+const fruitList = document.getElementById("fruit-list");
+const button = document.getElementById("my-button");
+
+const fruits = [
+  "banana",
+  "pear",
+  "apple"
+];
+
+// Récupérer un dataset
+const fruitListDataset = fruitList.dataset;
+
+function onButtonClicked() {
+  // margin-top -> marginTop
+  fruitList.style.marginTop = "100px";
+}
+
+// Ajouter un événement (au click)
+// button.addEventListener("click", onButtonClicked);
+
+// Créer de toute pièce un élément HTML
+// Ajouter du texte à cet elément HTML
+// Ajouter l'élément créé à ma liste de fruits
+
+// On boucle sur notre liste de string fruits
+for (const fruit of fruits) {
+  // Construit un élément HTML (li)
+  const fruitElement = document.createElement("li");
+
+  // Ajouter du texte à notre élément HTML
+  fruitElement.innerText = fruit;
+
+  // AJouter l'élément créé à son nœud parent
+  // fruitList.appendChild(fruitElement);
+}
+
+
+const fruitForm = document.getElementById("fruit-form");
+const fruitInput = document.getElementById("fruit-name");
+
+if (fruitForm === null) {
+  throw new Error("Unable to find the fruit form");
+}
+
+if (fruitInput === null) {
+  throw new Error("Unable to find the fruit input");
+}
+
+fruitForm.addEventListener("submit", function(event) {
+  // Empêche que le navigateur envoie le formulaire en HTTP
+  event.preventDefault();
+
+  // Récupérer le contenu que l'utilisateur a tapé
+  const fruitName = fruitInput.value;
+
+  // Création de l'élément HTML lié au nouveau fruit
+  const fruitElement = document.createElement("li");
+
+  // Ajout du nom du fruit à l'élément créé
+  fruitElement.innerText = fruitName;
+
+  // Ajouter l'élément HTML lié au fruit a notre liste de fruit
+  fruitList.appendChild(fruitElement);
+
+  // Réinitialise le formulaire
+  fruitForm.reset();
+
+  // Remettre le focus sur un input
+  fruitInput.focus();
+});

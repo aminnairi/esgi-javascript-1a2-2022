3b36be79f0c9763ca7308a019c329bff27bdcc8a
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-03-22 19:43:23 +0100

    wip lol table

--- chapitre-14/index.js
+++ chapitre-14/index.js
@@ -1,7 +1,21 @@
-console.log("XMLHttpRequest");
+const button = document.getElementById("get-champions");
 
-const request = new XMLHttpRequest();
+button.addEventListener("click", function() {
+  const request = new XMLHttpRequest();
+  
+  request.addEventListener("load", function() {
+    const xmlDocument = request.responseXML;
+    const xml = xmlDocument.firstElementChild;
+    const champions = xml.firstElementChild;
+    
+    console.log(champions);
+    
+    for (const champion of champions.children) {
+      const championName = champion.children[0].textContent;
+    }
+  });
 
-request.open("GET", "./lol.xml");
-
-request.send();
+  request.open("GET", "./lol.xml");
+  
+  request.send();
+});

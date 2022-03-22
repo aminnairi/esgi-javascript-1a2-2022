a390c50af2ac9fec8b234032a072ec89cd77a853
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-03-22 20:35:50 +0100

    added exercices

--- chapitre-14/index.js
+++ chapitre-14/index.js
@@ -1,21 +1,45 @@
 const button = document.getElementById("get-champions");
+const tableBody = document.getElementById("champions");
 
 button.addEventListener("click", function() {
   const request = new XMLHttpRequest();
   
   request.addEventListener("load", function() {
     const xmlDocument = request.responseXML;
-    const xml = xmlDocument.firstElementChild;
-    const champions = xml.firstElementChild;
+    const xmlTag = xmlDocument.firstElementChild;
+    const championsTag = xmlTag.firstElementChild;
     
-    console.log(champions);
-    
-    for (const champion of champions.children) {
-      const championName = champion.children[0].textContent;
+    for (const championTag of championsTag.children) {
+      const tableRow = document.createElement("tr");
+
+      for (const section of championTag.children) {
+        if (section.children.length === 0) {
+          const tableCell = document.createElement("td");
+          
+          tableCell.textContent = section.textContent;
+          
+          tableRow.appendChild(tableCell);
+
+          continue;
+        }
+        
+        for (const skill of section.children) {
+          const tableCell = document.createElement("td"); 
+          
+          tableCell.textContent = skill.textContent;
+          
+          tableRow.appendChild(tableCell);
+        }
+      } 
+      
+      tableBody.appendChild(tableRow);
     }
   });
-
+  
   request.open("GET", "./lol.xml");
   
   request.send();
 });
+
+// TODO: ajouter un champ de recherche pour chaque en-tête
+// Filtrer les lignes du tableau qui ne correspondent pas à la recherche

95cc21af23d7a64af0b071821dc57dbfcc9f4701
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-04-20 19:42:06 +0200

    added exercises correction

--- chapitre-14/index.js
+++ chapitre-14/index.js
@@ -1,10 +1,33 @@
 const button = document.getElementById("get-champions");
 const tableBody = document.getElementById("champions");
+const searchInputs = document.getElementsByClassName("search");
+
+for (let searchInputIndex = 0; searchInputIndex < searchInputs.length; searchInputIndex++) {
+  const searchInput = searchInputs[searchInputIndex];
+
+  searchInput.addEventListener("input", function() {
+    const searchInputValue = searchInput.value;
+
+    for (const row of tableBody.children) {
+      const cell = row.children[searchInputIndex];
+      const cellText = cell.innerText;
+      const isSearchMatching = cellText.toLowerCase().includes(searchInputValue.toLowerCase());
+
+      if (isSearchMatching) {
+        row.style.display = "table-row";
+      } else {
+        row.style.display = "none";
+      }
+    }
+  });
+}
 
 button.addEventListener("click", function() {
   const request = new XMLHttpRequest();
   
   request.addEventListener("load", function() {
+    tableBody.innerHTML = "";
+
     const xmlDocument = request.responseXML;
     const xmlTag = xmlDocument.firstElementChild;
     const championsTag = xmlTag.firstElementChild;
@@ -42,5 +65,4 @@ button.addEventListener("click", function() {
 });
 
 // TODO: ajouter un champ de recherche pour chaque en-tête
-// Filtrer les lignes du tableau qui ne correspondent pas à la recherche
-// TODO: faire en sorte que le bouton n'ajoute pas de duplicat de ligne
+// DONE: faire en sorte que le bouton n'ajoute pas de duplicat de ligne

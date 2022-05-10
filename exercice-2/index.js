f5d45e2f4a88e0be8d7058c395d482f96d0da070
aminnairi <18418459+aminnairi@users.noreply.github.com>
2022-05-10 19:15:29 +0200

    added initial code

--- exercice-2/index.js
+++ exercice-2/index.js
@@ -1,3 +1,5 @@
 // https://openweathermap.org/api
 // Champ de texte avec la possibilité de rentrer un nom de ville
 // À chaque fois que je clique sur un bouton "recherche", afficher la météo pour cette ville
+
+console.log("TODO");
